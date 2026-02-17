5b670c19e1ac18132bd4adf0320e881e41848df5
Christopher Henschel <chris@henschels.net>
2026-02-17 16:30:12 +0100

    fixed folders

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,8 +1,7 @@
 sudo docker network create caddy-proxy
 sudo docker network create database
-mkdir caddy/data caddy/config
 
 PG_PASS="$(openssl rand -base64 36 | tr -d '\n')"
 # Generate environment variables
 echo "PG_PASS=$PG_PASS" >> .env
-echo "AUTHENTIK_SECRET_KEY=$(openssl rand -base64 60 | tr -d '\n')" >> authentik/.env
+echo "AUTHENTIK_SECRET_KEY=$(openssl rand -base64 60 | tr -d '\n')" >> .env
