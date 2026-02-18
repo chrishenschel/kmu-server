a6216bdbb51ee6e3b770e7eb3f349ef9fdecd29b
Christopher Henschel <chris@henschels.net>
2026-02-18 09:50:51 +0100

    stash

--- dev-reset-install.sh
+++ dev-reset-install.sh
@@ -5,3 +5,5 @@ rm /synapse/data/*
 rm -rf /stalwart/data/etc
 rm -rf /stalwart/data/logs
 rm -rf /stalwart/data/data
+
+git stash && git pull && chmod a+x dev-reset-install.sh && chmod a+x 02-system-setup.sh
